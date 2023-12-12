78c4ded25a3d2edc02a56e175b129d7deeab020c
Revaz Komakhidze <rezokomakhidze1@gmail.com>
2023-12-12 20:57:10 +0400

    added find query

--- index.ts
+++ index.ts
@@ -3,7 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const main = async () => {
   const prisma = new PrismaClient();
 
-  await prisma.user.create({ data: { name: "name", email: "email" } });
+  await prisma.user.create({
+    data: { name: "name", email: "email", isAuthor: false },
+  });
+
+  await prisma.user.findFirst({ where: { id: "lasnda" } });
 };
 
 main().catch((e) => {
